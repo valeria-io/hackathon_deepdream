c2352cf2d5e031ebffb10f7fd22a4273e2390647
jackgrimes <jack.e.t.grimes@gmail.com>
2019-10-01 14:38:20 +0100

    initial commit

---
+++ configs.py
@@ -0,0 +1,7 @@
+input_video_path = r"C:\dev\data\hackathon_deepdream\input_video\jack.mp4"
+input_video_frames_path = r"C:\dev\data\hackathon_deepdream\input_video_frames"
+output_video_frames_path = r"C:\dev\data\hackathon_deepdream\output_video_frames"
+
+output_video_path = r"C:\dev\data\hackathon_deepdream\output_video"
+
+ext = "jpg"
