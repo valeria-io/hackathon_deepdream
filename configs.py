61e7286794e086fdcb86adc44522a867b37a2a3b
Tu  Xiao Chi <tuxiaochi@Tus-MacBook-Pro.local>
2019-10-03 11:14:09 +0100

    add os.listdir

--- configs.py
+++ configs.py
@@ -1,15 +1,19 @@
+import os
+
 windows = False
 
 if windows:
-    input_video_path = r"C:\dev\data\hackathon_deepdream\input_video\jack.mp4"
+    input_video_path = r"C:\dev\data\hackathon_deepdream\input_video"
     input_video_frames_path = r"C:\dev\data\hackathon_deepdream\input_video_frames"
     output_video_frames_path = r"C:\dev\data\hackathon_deepdream\output_video_frames"
     output_video_path = r"C:\dev\data\hackathon_deepdream\output_video"
 else:
-    input_video_path = r"/Users/marty/dev/data/hackathon_deepdream/input_video/carslow.mp4"
-    input_video_frames_path = r"/Users/marty/dev/data/hackathon_deepdream/input_video_frames"
-    output_video_frames_path = r"/Users/marty/dev/data/hackathon_deepdream/output_video_frames"
-    output_video_path = r"/Users/marty/dev/data/hackathon_deepdream/output_video"
-    output_video_frames_zooming_path = r"/Users/marty/dev/data/hackathon_deepdream/output_video_frames_zooming"
+    input_video_path = r"/Users/tuxiaochi/dev/data/hackathon_deepdream/input_video"
+    input_video_frames_path = r"/Users/tuxiaochi/dev/data/hackathon_deepdream/input_video_frames"
+    output_video_frames_path = r"/Users/tuxiaochi/dev/data/hackathon_deepdream/output_video_frames"
+    output_video_path = r"/Users/tuxiaochi/dev/data/hackathon_deepdream/output_video"
+    output_video_frames_zooming_path = r"/Users/tuxiaochi/dev/data/hackathon_deepdream/output_video_frames_zooming"
 
 ext = "jpg"
+
+input_video_path = os.path.join(input_video_path, os.listdir(input_video_path)[0])
